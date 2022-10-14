135365bd4b98c9be6582f65ebb4b46692885fda8
yuchan <74092268+H-yuchan@users.noreply.github.com>
2022-10-14 14:16:29 +0900

    Update pyShooting_v1.py
    미사일과 운석 충돌 시 터지는 모습 구현

--- pyShooting_v1.py
+++ pyShooting_v1.py
@@ -122,6 +122,18 @@ def runGame():
 
             rockX = random.randrange(0, padWidth - rockWidth)
             rockY = 0
+
+        if isShot:
+            drawObject(explosion, rockX, rockY)
+
+            rock = pygame.image.load(random.choice(rockImage))
+            rockSize = rock.get_rect().size
+            rockWidth = rockSize[0]
+            rockHeight = rockSize[1]
+            rockX = random.randrange(0, padWidth - rockWidth)
+            rockY = 0
+            isShot = False
+
         drawObject(rock, rockX, rockY)
 
         pygame.display.update()
